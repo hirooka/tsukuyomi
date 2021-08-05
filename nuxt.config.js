fd9fa05d5c5039b5be67dde9cb252da28da974d9
hirooka <hirooka@localhost>
2021-08-06 03:10:44 +0900

    add winston

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,5 +68,10 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    extend (config) {
+      config.node = {
+        fs: 'empty'
+      }
+    }
   }
 }
