d29d8f2ab22e9e2a7a2b7e1529ce21f61fe313d8
hirooka <hirooka@localhost>
2021-08-09 02:18:58 +0900

    add hls.js

---
+++ pages/player/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Player</h1>
+    <a href="/epg">EPG</a>
+    <video ref="video" width="100%" height="640" controls></video>
+  </div>
+</template>
+
+<script lang="ts">
+import Hls from 'hls.js'
+import Vue from 'vue'
+export default Vue.extend({
+  mounted () {
+    const video = this.$refs.video
+    const playlist = 'https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_16x9/gear4/prog_index.m3u8'
+    const hls = new Hls()
+    hls.loadSource(playlist)
+    hls.attachMedia(video)
+    hls.on(Hls.Events.MANIFEST_PARSED, function () {
+      video.play()
+    })
+  }
+})
+</script>
