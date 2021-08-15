54f3463c0814a6d1f79bf54411646f64dd502f66
hirooka <hirooka@localhost>
2021-08-15 21:58:17 +0900

    Fixed build

--- pages/epg/index.vue
+++ pages/epg/index.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <h1>EPG</h1>
-    <h2>{{date}}</h2>
+    <h2>{{ date }}</h2>
     <v-data-table
       :headers="headers"
       :items="programs"
       :items-per-page="10"
       class="elevation-1"
       @click:row="clickEvent"
-    ></v-data-table>
+    />
   </div>
 </template>
 
@@ -21,7 +21,7 @@ export default Vue.extend({
     const path = '/api/v1/programs/now'
     const response = await app.$axios.get(path)
     logger.debug(response)
-    const data = response.data.map((ele) => {
+    const data = response.data.map((ele: any) => {
       const begin = dayjs(ele.begin).format('HH:mm')
       ele.begin = begin
       const end = dayjs(ele.end).format('HH:mm')
@@ -45,7 +45,7 @@ export default Vue.extend({
     }
   },
   methods: {
-    clickEvent (selected) {
+    clickEvent (selected: any) {
       this.$store.commit('setProgram', selected)
       this.$store.commit('setCh', selected.channelRemoteControl)
       this.$router.push('/player')
--- pages/player/index.vue
+++ pages/player/index.vue
@@ -2,8 +2,8 @@
   <div>
     <h1>Player</h1>
     <a href="/epg">EPG</a>
-    <h2>{{ch}}</h2>
-    <video ref="video" width="100%" height="640" controls></video>
+    <h2>{{ ch }}</h2>
+    <video ref="video" width="100%" height="640" controls />
   </div>
 </template>
 
@@ -14,7 +14,7 @@ import logger from '~/plugins/logger'
 import { getters } from '~/store'
 export default Vue.extend({
   async asyncData ({ app, store, $config }) {
-    const ch = store.getters['ch'] as ReturnType<typeof getters.ch>
+    const ch = store.getters.ch as ReturnType<typeof getters.ch>
     const path = '/api/v1/izanami/start'
     const user = $config.hlsServerUsername
     const json = {
@@ -37,23 +37,23 @@ export default Vue.extend({
     logger.debug(response)
     store.commit('setM3u8', response.uri)
     return {
-      ch: ch,
+      ch,
       m3u8: response.uri
     }
   },
   mounted () {
-    const ch = this.$store.getters['ch'] as ReturnType<typeof getters.ch>
-    const program = this.$store.getters['program'] as ReturnType<typeof getters.program>
-    logger.debug(JSON.stringify(program))
+    const ch = this.$store.getters.ch as ReturnType<typeof getters.ch>
+    const program = this.$store.getters.program as ReturnType<typeof getters.program>
+    logger.debug(`${ch}: ${JSON.stringify(program)}`)
     const video = this.$refs.video
     // const playlist = 'https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_16x9/gear4/prog_index.m3u8'
-    const path = this.$store.getters['m3u8'] as ReturnType<typeof getters.m3u8>
+    const path = this.$store.getters.m3u8 as ReturnType<typeof getters.m3u8>
     const playlist = path
     const hls = new Hls()
     hls.loadSource(playlist)
-    hls.attachMedia(video)
+    hls.attachMedia(<HTMLMediaElement>video)
     hls.on(Hls.Events.MANIFEST_PARSED, function () {
-      video.play()
+      (<HTMLMediaElement>video).play()
     })
   }
 })
